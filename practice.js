cc5b91535cd8340b86f348b95aa9239a9271d693
navalraul <129669942+navalraul@users.noreply.github.com>
2023-05-14 17:00:16 +0530

    update

---
+++ practice.js
@@ -0,0 +1,19 @@
+// var Name = "Naval";
+// var num1 = 4;
+// var num2 = 5;
+
+
+// function more (a,b,c){
+//     var add = b+c;
+    
+//     console.log(`"${a}=${add}"`)
+    
+// }
+// more(Name,num1,num2)
+
+// arr = [16,17,9,0,1,2,3,1];
+// sum = 0;
+
+// for(i=0;i<arr.length;i++){
+//     sum+=arr[i]
+// }
